1bc182166fa0ce20a88eef1d7a8880826bf63bd0
Jihun Kim <theking461@snu.ac.kr>
2020-08-09 02:21:56 +0900

    fix

--- checkSites.py
+++ checkSites.py
@@ -4,7 +4,7 @@ import commands as cmd
 
 crabdir = cmd.getoutput('ls crab_projects')
 
-a = cmd.getoutput('crab status -d crab_projects/'+crabdir+' --long | grep -e finished -e failed -e toRetry -e transferring -e running | grep -e T2 -e T3')
+a = cmd.getoutput('crab status -d crab_projects/'+crabdir+' --long | grep -e finished -e failed -e toRetry -e transferring -e running | grep -e T2 -e T3 -e Unknown')
 
 siteLog = open("siteLog_"+crabdir+".txt", "w")
 siteLog.write(a)
